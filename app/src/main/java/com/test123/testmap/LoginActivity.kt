6438ef92efd870c56118acc29d2caf54103a877d
RifanDK <rifandanakusuma@gmail.com>
2024-06-19 10:16:30 +0700

    update 1

---
+++ app/src/main/java/com/test123/testmap/LoginActivity.kt
@@ -0,0 +1,5 @@
+package com.test123.testmap
+
+class LoginActivity {
+
+}
---
+++ app/src/main/java/com/test123/testmap/LoginData.kt
@@ -0,0 +1,7 @@
+package com.test123.testmap
+
+data class LoginData(
+    val loginusername: String,
+    val loginpassword: String
+
+)
